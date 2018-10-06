fb9e40038b485d1ab993bbd2c8141af9f30df66a
Unknown <sebastian.gallard.13@sansano.usm.cl>
2018-10-06 16:23:28 -0300

    fix notification backend

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -33,38 +33,38 @@ export class HomePage {
   }
 
   ionViewDidLoad(){
-    var socket = io.connect('http://easy.notification.boldware.cl/');
-    const httpOptions = {
-      headers: new HttpHeaders({'Content-Type': 'application/x-www-form-urlencoded','connectionId': '5b3320',
-      'X-AUTH-TOKEN': 'L7S9O5M4T1I' })
-    };
-    this.http.put('http://easy.notification.boldware.cl/api/5b33/register',
-    { 
-    }, httpOptions).subscribe(
-      (data) => {
-        socket.on('connect', function() {
-        socket.emit('register', '5b33', '5b3320');
-        });
-      }
-    );
-    socket.on('message', function(msg) {
-      console.log(JSON.parse(msg).title);
-      console.log(JSON.parse(msg).body);
-      console.log(msg);
-      //handle your message
+    // var socket = io.connect('http://easy.notification.boldware.cl/');
+    // const httpOptions = {
+    //   headers: new HttpHeaders({'Content-Type': 'application/x-www-form-urlencoded','connectionId': '5b3320',
+    //   'X-AUTH-TOKEN': 'L7S9O5M4T1I' })
+    // };
+    // this.http.put('http://easy.notification.boldware.cl/api/5b33/register',
+    // { 
+    // }, httpOptions).subscribe(
+    //   (data) => {
+    //     socket.on('connect', function() {
+    //     socket.emit('register', '5b33', '5b3320');
+    //     });
+    //   }
+    // );
+    // socket.on('message', function(msg) {
+    //   console.log(JSON.parse(msg).title);
+    //   console.log(JSON.parse(msg).body);
+    //   console.log(msg);
+    //   //handle your message
 
-      swal(JSON.parse(msg).title,JSON.parse(msg).body,"success");
-      this.localNotifications.schedule({
-        id: 5,
-        title: JSON.parse(msg).title,
-        text: JSON.parse(msg).body,
-        // title: "Hola",
-        // text: "HAAAAAAAAAAAAAAA",
-        priority: 2
-        //   sound: isAndroid? 'file://sound.mp3': 'file://beep.caf',
-        //   data: { secret: key }
-        });
-      });
+    //   swal(JSON.parse(msg).title,JSON.parse(msg).body,"success");
+    //   this.localNotifications.schedule({
+    //     id: 5,
+    //     title: JSON.parse(msg).title,
+    //     text: JSON.parse(msg).body,
+    //     // title: "Hola",
+    //     // text: "HAAAAAAAAAAAAAAA",
+    //     priority: 2
+    //     //   sound: isAndroid? 'file://sound.mp3': 'file://beep.caf',
+    //     //   data: { secret: key }
+    //     });
+    //   });
    }
 
 }
