d84d3810911095f2c541589220c75241fa304f07
Benjamin Miranda <benjamin.miranda.12@sansano.usm.cl>
2018-10-23 00:37:56 -0300

    add toastr on ivite list

--- src/pages/guests-list/guests-list.ts
+++ src/pages/guests-list/guests-list.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import swal from 'sweetalert';
 import {BackendProvider} from '../../providers/backend/backend';
+import { ToastController } from 'ionic-angular';
 
 /**
  * Generated class for the GuestsListPage page.
@@ -18,11 +19,12 @@ import {BackendProvider} from '../../providers/backend/backend';
 export class GuestsListPage {
   items:any;
   toggle: boolean;
-
+  toast: any;
   constructor(
     public navCtrl: NavController, 
     public navParams: NavParams,
-    public backend: BackendProvider) {
+    public backend: BackendProvider,
+    public toastCtrl: ToastController) {
       this.backend.GUESTS_BY_USER().then(
         data => {
           this.items = data['data'];
@@ -34,11 +36,35 @@ export class GuestsListPage {
   ionViewDidLoad() {
     console.log('ionViewDidLoad GuestsListPage');
   }
+  presentToast(value) {
 
+
+    if(value){
+      this.toast = this.toastCtrl.create({
+        message: 'Acceso concedido con éxito',
+        duration: 3000,
+        position: 'top'
+      });
+    
+    }else{
+      this.toast = this.toastCtrl.create({
+        message: 'Acceso denegado con éxito!',
+        duration: 3000,
+        position: 'top'
+      });
+      
+    }
+    
+    this.toast.onDidDismiss(() => {
+      //console.log('Dismissed toast');
+    });
+    this.toast.present();
+}
   update(id,val){
     console.log(val);
     this.backend.CHANGE_THIRD_ACCESS(id,val).then(
       data => {
+        this.presentToast(val);
         console.log(data);
         
       }
